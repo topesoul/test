1a682a4eec1dc015af63750aec4bb4c4729cae65
topesoul <topesoul@gmail.com>
2024-04-07 10:07:20 +0000

    feat: amend js comments

--- cleanedassets/js/script.js
+++ cleanedassets/js/script.js
@@ -3,7 +3,7 @@ var swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
 
-    // If we need pagination
+    // pagination
     pagination: {
         el: '.swiper-pagination',
     },
@@ -14,7 +14,7 @@ var swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
 
-    // And if we need scrollbar
+    // scrollbar
     scrollbar: {
         el: '.swiper-scrollbar',
     },
